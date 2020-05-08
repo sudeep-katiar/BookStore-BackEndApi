df65be5e30da261a3b306da6ca17bf23c97ef78d
durgasankar <durgasankar.raja500@gmail.com>
2020-05-08 23:00:55 +0530

    [durgasankar].add -> registartion failure for already registered user

--- BookStore-UserApi/src/test/java/com/bookstore/user/services/UserServiceTest.java
+++ BookStore-UserApi/src/test/java/com/bookstore/user/services/UserServiceTest.java
@@ -7,12 +7,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bookstore.user.configuration.PasswordEncoderConfiguration;
 import com.bookstore.user.dao.IAddressRepository;
 import com.bookstore.user.dao.IUserDAO;
+import com.bookstore.user.exception.UserAlradyRegisterException;
 import com.bookstore.user.model.User;
 import com.bookstore.user.response.UserData;
 import com.bookstore.user.response.UserResponse;
@@ -42,24 +44,29 @@ public class UserServiceTest {
 
 	@Test
 	public void sucessful_registration_user() {
-		User user = new User();
-		user.setPassword("password");
-		user.setEmail("ihuihs");
-		user.setUId(5);
-
-		user.setSeller(false);
+		User newUser = new User();
+		newUser.setPassword("password");
+		newUser.setEmail("validEmail");
+		newUser.setUId(5);
+		newUser.setSeller(false);
 
 //		Mockito.doReturn(null).when(userDao).isUserExist(user);
 //		Mockito.when(passwordEncryption.passwordEncoder().encode(Mockito.anyString())).thenReturn("jgj");
 		Mockito.when(passwordEncryption.passwordEncoder()).thenReturn(bCryptPasswordEncoder);
-		Mockito.when(bCryptPasswordEncoder.encode(Mockito.anyString())).thenReturn("Password");
+		Mockito.when(bCryptPasswordEncoder.encode(Mockito.anyString())).thenReturn("encodedPassword");
 //		Mockito.when(userDao.register(user)).thenReturn(1);
 //		Mockito.doReturn("token").when(generateToken).generateToken(Mockito.anyLong());
 //		Mockito.doNothing().when(emailGenerate).sendEmail(Mockito.anyString(), Mockito.anyString(),
 //				Mockito.anyString());
-
-		ResponseEntity<UserResponse> register = userServiceImpl.register(user);
-		Assert.assertEquals(register.getStatusCode().value(), 202);
+		ResponseEntity<UserResponse> registrationResponse = userServiceImpl.register(newUser);
+		Assert.assertEquals(registrationResponse.getStatusCode().value(), HttpStatus.ACCEPTED.value());
+	}
+	
+	@Test(expected = UserAlradyRegisterException.class)
+	public void registration_failure_exception_when_user_already_exist() {
+		User newUser = new User();
+		Mockito.when(userDao.isUserExist(newUser)).thenReturn(new User());
+		userServiceImpl.register(newUser);
 	}
 
 }
