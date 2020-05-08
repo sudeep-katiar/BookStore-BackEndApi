ef26520d9836698bdfe6387d446dc7e7cb25898a
durgasankar <durgasankar.raja500@gmail.com>
2020-05-08 16:18:15 +0530

    [durgasankar].add -> test case for get addresses with all valid and invalid inputs

--- BookStore-UserApi/src/test/java/com/bookstore/user/controller/UserControllerTest.java
+++ BookStore-UserApi/src/test/java/com/bookstore/user/controller/UserControllerTest.java
@@ -1,5 +1,8 @@
 package com.bookstore.user.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +21,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.bookstore.user.dto.AddressDto;
+import com.bookstore.user.model.UserAddress;
 import com.bookstore.user.service.IUserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -90,5 +94,47 @@ public class UserControllerTest {
 		 
 		 log.info("fetch result : " + fetchedResponse.getContentAsString());
 	     Assert.assertEquals ("Checking failure scenario of add address", fetchedResponse.getStatus(), HttpStatus.BAD_REQUEST.value());
+	}
+	
+	@Test
+	public void get_addresses_test_for_valid_addresses() throws Exception {
+		UserAddress address1 = new UserAddress();
+		address1.setCountry("India");
+		address1.setState("Odisha");
+		address1.setPinCode(756045);
+		UserAddress address2 = new UserAddress();
+		address2.setCountry("USA");
+		address2.setState("New York");
+		address2.setPinCode(100458);
+		List<UserAddress> addresses = new ArrayList<>();
+		addresses.add(address1);
+		addresses.add(address2);
+		log.info("created addresses : " + addresses.toString());
+		String getAddressesUrl = "/users/address/get";
+			MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get(getAddressesUrl)
+				.header("token", "validToken")
+				.contentType(MediaType.APPLICATION_JSON);
+		
+		Mockito.when(userService.getAllAddressOfUser(Mockito.anyString())).thenReturn(addresses);
+	
+		 MockHttpServletResponse fetchedResponse = mockMvc.perform(requestBuilder)
+				.andReturn()
+				.getResponse();
+		 
+		 log.info("fetch result : " + fetchedResponse.getContentAsString());
+	     Assert.assertEquals ("all Addresses : ", fetchedResponse.getStatus(), HttpStatus.OK.value());
+	     
+	     List<UserAddress> fetchAddresses = userService.getAllAddressOfUser(Mockito.anyString());
+	     Assert.assertTrue ("is Data fetched in response is matching with exert value: ", fetchAddresses.size() == 2);
+	     Assert.assertFalse ("checking with false condition of fetched data size: ", fetchAddresses.size() == 1);
+	     
+	    String fetchedCountryName = fetchAddresses.stream()
+	     		.filter(address -> address.getPinCode() == 756045)
+	     		.findFirst()
+	     		.get()
+	     		.getCountry();
+	     Assert.assertEquals ("checking with value inside response: ", fetchedCountryName, "India");
+		    
 	}
 }
