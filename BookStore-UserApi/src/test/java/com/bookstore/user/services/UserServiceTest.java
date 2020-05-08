024b7f40a6f6a0566146dcfbfec6e6c7936341c0
durgasankar <durgasankar.raja500@gmail.com>
2020-05-09 00:15:08 +0530

    [durgasankar].add -> activate user account test sucess for both positive and negative user token supply

--- BookStore-UserApi/src/test/java/com/bookstore/user/services/UserServiceTest.java
+++ BookStore-UserApi/src/test/java/com/bookstore/user/services/UserServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.bookstore.user.configuration.PasswordEncoderConfiguration;
 import com.bookstore.user.dao.IAddressRepository;
 import com.bookstore.user.dao.IUserDAO;
+import com.bookstore.user.exception.InvalidTokenOrExpiredException;
 import com.bookstore.user.exception.UserAlradyRegisterException;
 import com.bookstore.user.model.User;
 import com.bookstore.user.response.UserData;
@@ -22,7 +23,10 @@ import com.bookstore.user.service.UserServiceImpl;
 import com.bookstore.user.util.EmailGenerator;
 import com.bookstore.user.util.JwtTokenUtil;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RunWith(MockitoJUnitRunner.class)
+@Slf4j
 public class UserServiceTest {
 
 	@InjectMocks
@@ -67,6 +71,30 @@ public class UserServiceTest {
 		User newUser = new User();
 		Mockito.when(userDao.isUserExist(newUser)).thenReturn(new User());
 		userServiceImpl.register(newUser);
+	}
+	
+	@Test
+	public void activate_user_with_positive_token() {
+		String token = "validToken";
+		int idValueGraterThanZero = 2;
+		int afterActivationReturnValue = 1;
+		Mockito.when(generateToken.parseToken(token))
+			.thenReturn(idValueGraterThanZero);
+		Mockito.when(userDao.activateUSer(idValueGraterThanZero)).thenReturn(afterActivationReturnValue);
+		ResponseEntity<UserResponse> varificationResponse = userServiceImpl.activateUser(token);
+		log.info("verification response : " + varificationResponse);
+		Assert.assertTrue(varificationResponse.getStatusCodeValue() == HttpStatus.ACCEPTED.value());
+	}
+	
+	@Test(expected = InvalidTokenOrExpiredException.class)
+	public void activate_user_with_invalid_token_throws_exception() {
+		String token = "invalidToken";
+		int userIdFetched = 2;
+		int returnValueOfUnsucessfulQueryExecution = 0;
+		Mockito.when(generateToken.parseToken(token))
+			.thenReturn(userIdFetched);
+		Mockito.when(userDao.activateUSer(userIdFetched)).thenReturn(returnValueOfUnsucessfulQueryExecution);
+		userServiceImpl.activateUser(token);
 	}
 
 }
