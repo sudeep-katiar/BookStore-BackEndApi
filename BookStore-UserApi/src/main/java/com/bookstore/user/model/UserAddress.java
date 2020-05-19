d639206cbcd6155cff5ad98581fcf6b4895d61af
Rupeshp <49507248+patilrupesh990@users.noreply.github.com>
2020-05-19 17:47:39 +0530

    [Rupesh Patil].Updated UserAddress entity

--- BookStore-UserApi/src/main/java/com/bookstore/user/model/UserAddress.java
+++ BookStore-UserApi/src/main/java/com/bookstore/user/model/UserAddress.java
@@ -18,28 +18,28 @@ import javax.validation.constraints.NotBlank;
 @Table(name = "users_addresses")
 @Entity
 public class UserAddress {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ADDRESS_ID")
-    private long addressId;
-    @Column(length = 25, name = "STREET")
-    private String street;
-    @Column(length = 25, name = "TOWN")
-    private String town;
-    @Column(length = 25, name = "DISTRICT")
-    private String district;
-    @Column(length = 25, name = "STATE")
-    private String state;
-    @Column(length = 25, name = "COUNTRY")
-    @NotBlank(message = "Country name is mandatory")
-    private String country;
-    @Column(name = "PIN_CODE", nullable = false)
-    @NotBlank(message = "Pin code is mandatory")
-    private int pinCode;
-    @Column(length = 25, name = "CUSTOMER_NAME")
-    private String customerName;
-    @Column(length = 25, name = "CONTACT")
-    private String contact;
-    @Column(length = 25, name = "ADDRESS_TYPE")
-    private String type;
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "ADDRESS_ID")
+	    private long addressId;
+	    @Column(length = 25, name = "LOCALITY")
+	    private String locality;
+	    @Column(length = 25, name = "ADDRESS")
+	    private String address;
+	    @Column( name = "CITY")
+	    private String city;
+	    @Column(length = 25, name = "LANDMARK")
+	    private String landMark;
+	    @Column(length = 25, name = "COUNTRY")
+	    @NotBlank(message = "Country name is mandatory")
+	    private String country;
+	    @Column(name = "PIN_CODE", nullable = false)
+	    @NotBlank(message = "Pin code is mandatory")
+	    private int pinCode;
+	    @Column(length = 25, name = "CUSTOMER_NAME")
+	    private String customerName;
+	    @Column(length = 25, name = "CONTACT")
+	    private String contact;
+	    @Column(length = 25, name = "ADDRESS_TYPE")
+	    private String type;
 }
